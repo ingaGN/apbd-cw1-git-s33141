4d2fd670c45d604b89ece4cb9b15f3c43437a310
Inga Gadomska-Nguyen <s33141@pjwstk.edu.pl>
2026-03-18 23:56:36 +0100

    feat: nowa metoda do pobrania danych uzytkownika

--- program.java
+++ program.java
@@ -1,5 +1,10 @@
 public class Main{
     public static void main() {
         System.out.println("hello!");
+
+    }
+
+    public void getName(){
+        System.out.printf("enter your name: ");
     }
 }
